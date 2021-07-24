1876b9e6ae609c2b99d633524f69a79485257aa3
Willsem <happymainer@gmail.com>
2021-07-25 02:15:56 +0300

    make CombineResults function

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,3 +1,55 @@
 package main
 
-// сюда писать код
+import (
+	"sort"
+	"strings"
+	"sync"
+)
+
+const (
+	maxWorkers = 100
+	th         = 6
+)
+
+func ExecutePipeline(jobs ...job) {
+	in := make(chan interface{}, maxWorkers)
+	wg := &sync.WaitGroup{}
+
+	for _, job := range jobs {
+		out := make(chan interface{}, maxWorkers)
+		wg.Add(1)
+		go jobWorker(job, in, out, wg)
+		in = out
+	}
+
+	wg.Wait()
+}
+
+func jobWorker(job job, in, out chan interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(out)
+	job(in, out)
+}
+
+func SingleHash(in, out chan interface{}) {
+}
+
+func MultiHash(in, out chan interface{}) {
+}
+
+func CombineResults(in, out chan interface{}) {
+	var result []string
+
+	for data := range in {
+		dataString, err := data.(string)
+
+		if !err {
+			panic("CombineResults: data from input not a string")
+		}
+
+		result = append(result, dataString)
+	}
+
+	sort.Strings(result)
+	out <- strings.Join(result, "_")
+}
