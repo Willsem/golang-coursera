5f303eecb8576102f33940c20d1dc0268b3b9b1a
Willsem <happymainer@gmail.com>
2021-08-14 18:03:52 +0300

    add methods to explorer

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -1,4 +1,63 @@
 package main
 
-// тут вы пишете код
-// обращаю ваше внимание - в этом задании запрещены глобальные переменные
+import (
+	"database/sql"
+	"net/http"
+	"regexp"
+)
+
+var (
+	tableRegexp       = regexp.MustCompile(`^\/[^\/]+\/?$`)
+	tableWithIdRegexp = regexp.MustCompile(`^\/[^\/]+\/.+`)
+)
+
+type DbExplorer struct {
+	db *sql.DB
+}
+
+func NewDbExplorer(db *sql.DB) (DbExplorer, error) {
+	return DbExplorer{db: db}, nil
+}
+
+func (explorer DbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == "GET" && r.URL.Path == "/":
+		explorer.getTables(w, r)
+	case r.Method == "GET" && tableRegexp.MatchString(r.URL.Path):
+		explorer.getRowsFromTable(w, r)
+	case r.Method == "GET" && tableWithIdRegexp.MatchString(r.URL.Path):
+		explorer.getRowFromTable(w, r)
+	case r.Method == "PUT" && tableRegexp.MatchString(r.URL.Path):
+		explorer.putRowToTable(w, r)
+	case r.Method == "POST" && tableWithIdRegexp.MatchString(r.URL.Path):
+		explorer.postRowInTable(w, r)
+	case r.Method == "DELETE" && tableWithIdRegexp.MatchString(r.URL.Path):
+		explorer.deleteRowFromTable(w, r)
+	default:
+		http.Error(w, "unknown method", http.StatusNotFound)
+	}
+}
+
+// GET /
+func (explorer DbExplorer) getTables(w http.ResponseWriter, r *http.Request) {
+}
+
+// GET /$table?limit=5&offset=7
+func (explorer DbExplorer) getRowsFromTable(w http.ResponseWriter, r *http.Request) {
+}
+
+// GET /$table/$id
+func (explorer DbExplorer) getRowFromTable(w http.ResponseWriter, r *http.Request) {
+}
+
+// PUT /$table
+func (explorer DbExplorer) putRowToTable(w http.ResponseWriter, r *http.Request) {
+}
+
+// POST /$table/$id
+func (explorer DbExplorer) postRowInTable(w http.ResponseWriter, r *http.Request) {
+}
+
+// DELETE /$table/$id
+func (explorer DbExplorer) deleteRowFromTable(w http.ResponseWriter, r *http.Request) {
+}
