201e1c46c1e16bdd452b2da0d7162249b0bd137a
Willsem <happymainer@gmail.com>
2021-10-21 23:02:57 +0300

    fix service

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -12,222 +12,373 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/peer"
-	"google.golang.org/grpc/status"
 )
 
-func authStreamInterceptor(
-	srv interface{},
-	ss grpc.ServerStream,
-	info *grpc.StreamServerInfo,
-	handler grpc.StreamHandler,
-) error {
-	md, _ := metadata.FromIncomingContext(ss.Context())
-	p, _ := peer.FromContext(ss.Context())
-	consumer, ok := md["consumer"]
-	m, _ := srv.(*Service)
-	if !ok || !m.hasAccess(consumer[0], info.FullMethod) {
-		return status.Error(codes.Unauthenticated, "No consumer information in request")
-	}
-	m.addInvocation(info.FullMethod, consumer[0])
-	m.logEvent(Event{Timestamp: time.Now().UnixNano(), Consumer: consumer[0], Method: info.FullMethod, Host: p.Addr.String()})
-	return handler(srv, ss)
+const (
+	bizAdmin = "biz_admin"
+	bizUser  = "biz_user"
+	logger   = "logger"
+)
+
+var aclStorage map[string]json.RawMessage
+
+type service struct {
+	m                    *sync.RWMutex
+	incomingLogsCh       chan *logMsg
+	closeListenersCh     chan struct{}
+	listeners            []*listener
+	aclStorage           map[string][]string
+	statListeners        []*statListener
+	incomingStatCh       chan *statMsg
+	closeStatListenersCh chan struct{}
 }
 
-func authInterceptor(
-	ctx context.Context,
-	req interface{},
-	info *grpc.UnaryServerInfo,
-	handler grpc.UnaryHandler,
-) (interface{}, error) {
-
-	md, _ := metadata.FromIncomingContext(ctx)
-	p, _ := peer.FromContext(ctx)
-	consumer, ok := md["consumer"]
-	m, _ := info.Server.(*Service)
-	fmt.Printf("method %s \n", info.FullMethod)
-	if !ok || !m.hasAccess(consumer[0], info.FullMethod) {
-		return nil, status.Error(codes.Unauthenticated, "No consumer information in request")
-	}
-
-	reply, err := handler(ctx, req)
-	m.addInvocation(info.FullMethod, consumer[0])
-	m.logEvent(Event{
-		Timestamp: time.Now().UnixNano(),
-		Consumer:  consumer[0],
-		Method:    info.FullMethod,
-		Host:      p.Addr.String(),
-	})
-
-	return reply, err
+type logMsg struct {
+	methodName   string
+	consumerName string
 }
 
-type Service struct {
-	AclData      map[string][]string
-	LogMutex     *sync.RWMutex
-	LogListeners map[int]chan *Event
-	LogCount     int
-	StatMutex    *sync.RWMutex
-	StatCount    int
-	Stat         map[int]Stat
+type listener struct {
+	logsCh  chan *logMsg
+	closeCh chan struct{}
 }
 
-func (m *Service) registerLogClient() (int, chan *Event) {
-	m.LogMutex.Lock()
-	defer m.LogMutex.Unlock()
-	m.LogCount++
-	m.LogListeners[m.LogCount] = make(chan *Event)
-	return m.LogCount, m.LogListeners[m.LogCount]
+type statMsg struct {
+	methodName   string
+	consumerName string
 }
 
-func (m *Service) logEvent(event Event) {
-	fmt.Printf("Logging event %#v \n", event)
-	m.LogMutex.RLock()
-	defer m.LogMutex.RUnlock()
-	for client, c := range m.LogListeners {
-		fmt.Printf("Notification to client %v\n", client)
-		c <- &event
-	}
+type statListener struct {
+	statCh  chan *statMsg
+	closeCh chan struct{}
 }
 
-func (m *Service) unsubscribeLogClient(clientId int) {
-	fmt.Printf("Unsubscribing client %v\n", clientId)
-	m.LogMutex.Lock()
-	defer m.LogMutex.Unlock()
-	delete(m.LogListeners, clientId)
-}
-
-func (m *Service) registerStatClient() int {
-	m.StatMutex.Lock()
-	defer m.StatMutex.Unlock()
-	m.StatCount++
-	m.Stat[m.StatCount] = Stat{ByConsumer: make(map[string]uint64), ByMethod: make(map[string]uint64)}
-	return m.StatCount
-}
-
-func (m *Service) unsubscribeStatClient(client int) {
-	m.StatMutex.Lock()
-	defer m.StatMutex.Unlock()
-	delete(m.Stat, client)
-}
-
-func (m *Service) resetStat(client int) {
-	m.StatMutex.Lock()
-	defer m.StatMutex.Unlock()
-	m.Stat[client] = Stat{ByConsumer: make(map[string]uint64), ByMethod: make(map[string]uint64)}
-}
-
-func (m *Service) getStat(client int) Stat {
-	m.StatMutex.RLock()
-	defer m.StatMutex.RUnlock()
-	s := m.Stat[client]
-	s.Timestamp = time.Now().UnixNano()
-	return s
-}
-
-func (m *Service) addInvocation(method string, consumer string) {
-	m.StatMutex.Lock()
-	defer m.StatMutex.Unlock()
-	for _, stat := range m.Stat {
-		stat.ByMethod[method]++
-		stat.ByConsumer[consumer]++
-	}
-}
-
-func (m *Service) hasAccess(consumerName string, methodName string) bool {
-	fmt.Printf("checking method %s access for conxumer %s \n", methodName, consumerName)
-	methods, ok := m.AclData[consumerName]
+func getConsumerNameFromContext(ctx context.Context) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return false
+		return "", grpc.Errorf(codes.Unauthenticated, "can not get metadata")
 	}
-	for _, method := range methods {
-		if methodName == method {
-			return true
+	consumer, ok := md["consumer"]
+	if !ok || len(consumer) != 1 {
+		return "", grpc.Errorf(codes.Unauthenticated, "can not get metadata")
+	}
+
+	return consumer[0], nil
+}
+
+func (srv *service) checkBizPermission(consumer, method string) error {
+	allowedMethods, ok := srv.aclStorage[consumer]
+	if !ok {
+		return grpc.Errorf(codes.Unauthenticated, "permission denied")
+	}
+
+	for _, m := range allowedMethods {
+		//check if everything allowed
+		splitted := strings.Split(m, "/")
+		if len(splitted) == 3 && splitted[2] == "*" {
+			return nil
 		}
-		if strings.HasSuffix(method, "*") {
-			prefix := strings.TrimSuffix(method, "*")
-			if strings.HasPrefix(methodName, prefix) {
-				return true
+
+		if m == method {
+			return nil
+		}
+	}
+
+	return grpc.Errorf(codes.Unauthenticated, "permission denied")
+}
+
+func parseACL(acl string) (map[string][]string, error) {
+	var aclParsed map[string]*json.RawMessage
+	result := make(map[string][]string)
+
+	err := json.Unmarshal([]byte(acl), &aclParsed)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range aclParsed {
+		var val []string
+		err := json.Unmarshal(*v, &val)
+		if err != nil {
+			return nil, err
+		}
+
+		result[k] = val
+	}
+
+	return result, nil
+}
+
+func (srv *service) addListener(l *listener) {
+	srv.m.Lock()
+	srv.listeners = append(srv.listeners, l)
+	srv.m.Unlock()
+}
+
+func (srv *service) logsSender() {
+	for {
+		select {
+		case log := <-srv.incomingLogsCh:
+			srv.m.RLock()
+			for _, l := range srv.listeners {
+				l.logsCh <- log
 			}
+			srv.m.RUnlock()
+
+		case <-srv.closeListenersCh:
+			srv.m.RLock()
+			for _, l := range srv.listeners {
+				l.closeCh <- struct{}{}
+			}
+			srv.m.RUnlock()
+
+			return
 		}
 	}
-	return false
 }
 
-func newService() *Service {
-	result := new(Service)
-	result.LogMutex = &sync.RWMutex{}
-	result.LogListeners = make(map[int]chan *Event)
-	result.StatMutex = &sync.RWMutex{}
-	result.Stat = make(map[int]Stat)
-	return result
+func (srv *service) statsSender() {
+	for {
+		select {
+		case statMsg := <-srv.incomingStatCh:
+			srv.m.RLock()
+			for _, l := range srv.statListeners {
+				l.statCh <- statMsg
+			}
+			srv.m.RUnlock()
+
+		case <-srv.closeStatListenersCh:
+			srv.m.RLock()
+			for _, l := range srv.statListeners {
+				l.closeCh <- struct{}{}
+			}
+			srv.m.RUnlock()
+			return
+		}
+	}
 }
 
-func StartMyMicroservice(context context.Context, listendAddress string, aclData string) error {
-	Service := newService()
-	err := json.Unmarshal([]byte(aclData), &Service.AclData)
+func (srv *service) addStatListener(sl *statListener) {
+	srv.m.Lock()
+	srv.statListeners = append(srv.statListeners, sl)
+	srv.m.Unlock()
+}
+
+func StartMyMicroservice(ctx context.Context, addr, acl string) error {
+	aclParsed, err := parseACL(acl)
 	if err != nil {
 		return err
 	}
-	lis, err := net.Listen("tcp", listendAddress)
-	server := grpc.NewServer(grpc.UnaryInterceptor(authInterceptor), grpc.StreamInterceptor(authStreamInterceptor))
 
-	RegisterAdminServer(server, Service)
-	RegisterBizServer(server, Service)
-	go func() {
-		select {
-		case <-context.Done():
-			println("closing server")
-			server.Stop()
-		}
-	}()
-	println("Starting server on ", listendAddress)
-	go func() {
-		server.Serve(lis)
-	}()
-	return err
-}
-
-func (*Service) Check(context.Context, *Nothing) (*Nothing, error) {
-	return &Nothing{Dummy: true}, nil
-}
-
-func (*Service) Add(context.Context, *Nothing) (*Nothing, error) {
-	return &Nothing{Dummy: true}, nil
-}
-
-func (*Service) Test(context.Context, *Nothing) (*Nothing, error) {
-	return &Nothing{Dummy: true}, nil
-}
-
-func (m *Service) Logging(nothing *Nothing, server Admin_LoggingServer) error {
-	fmt.Printf("Logging \n")
-	clientId, notificationChanel := m.registerLogClient()
-	fmt.Printf("Registered log client %d \n", clientId)
-	for {
-		msg := <-notificationChanel
-		fmt.Printf("sending msg %#v to client %v", msg, clientId)
-		err := server.Send(msg)
-		if err != nil {
-			return err
-		}
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(fmt.Sprintf("can not start the service. %s", err.Error()))
 	}
 
+	service := &service{
+		m:                    &sync.RWMutex{},
+		incomingLogsCh:       make(chan *logMsg, 0),
+		listeners:            make([]*listener, 0),
+		aclStorage:           aclParsed,
+		closeListenersCh:     make(chan struct{}),
+		statListeners:        make([]*statListener, 0),
+		incomingStatCh:       make(chan *statMsg, 0),
+		closeStatListenersCh: make(chan struct{}),
+	}
+
+	go service.logsSender()
+	go service.statsSender()
+
+	opts := []grpc.ServerOption{grpc.UnaryInterceptor(service.unaryInterceptor),
+		grpc.StreamInterceptor(service.streamInterceptor)}
+
+	srv := grpc.NewServer(opts...)
+	fmt.Println("starting server at: ", addr)
+
+	RegisterBizServer(srv, service)
+	RegisterAdminServer(srv, service)
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			service.closeListenersCh <- struct{}{}
+
+			service.closeStatListenersCh <- struct{}{}
+
+			srv.Stop()
+			return
+		}
+	}()
+
+	go func() {
+		err := srv.Serve(lis)
+		if err != nil {
+			panic(err)
+		}
+		return
+	}()
+
+	return nil
 }
 
-func (m *Service) Statistics(statInterval *StatInterval, server Admin_StatisticsServer) error {
-	fmt.Printf("Statistics ")
-	interval := statInterval.IntervalSeconds
-	ticker := time.NewTicker(time.Duration(interval) * time.Second)
-	clientId := m.registerStatClient()
+func (s *service) unaryInterceptor(ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (interface{}, error) {
+	consumer, err := getConsumerNameFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.checkBizPermission(consumer, info.FullMethod)
+	if err != nil {
+		return nil, err
+	}
+
+	logMsg := logMsg{
+		consumerName: consumer,
+		methodName:   info.FullMethod,
+	}
+
+	s.incomingLogsCh <- &logMsg
+
+	statMsg := statMsg{
+		consumerName: consumer,
+		methodName:   info.FullMethod,
+	}
+
+	s.incomingStatCh <- &statMsg
+
+	h, err := handler(ctx, req)
+	return h, err
+}
+
+func (s *service) streamInterceptor(srv interface{},
+	ss grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler) error {
+	consumer, err := getConsumerNameFromContext(ss.Context())
+	if err != nil {
+		return err
+	}
+
+	err = s.checkBizPermission(consumer, info.FullMethod)
+	if err != nil {
+		return err
+	}
+
+	if info.FullMethod == "/main.Admin/Logging" {
+		msg := logMsg{
+			consumerName: consumer,
+			methodName:   info.FullMethod,
+		}
+		s.m.RLock()
+		for _, l := range s.listeners {
+			l.logsCh <- &msg
+		}
+		s.m.RUnlock()
+
+	} else {
+		msg := statMsg{
+			consumerName: consumer,
+			methodName:   info.FullMethod,
+		}
+
+		s.m.RLock()
+		for _, l := range s.statListeners {
+			l.statCh <- &msg
+		}
+		s.m.RUnlock()
+
+	}
+
+	return handler(srv, ss)
+}
+
+func (s *service) Check(ctx context.Context, n *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (s *service) Add(ctx context.Context, n *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (s *service) Test(ctx context.Context, n *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (s *service) Logging(nothing *Nothing, srv Admin_LoggingServer) error {
+
+	listener := listener{
+		logsCh:  make(chan *logMsg),
+		closeCh: make(chan struct{}),
+	}
+	s.addListener(&listener)
+
 	for {
-		<-ticker.C
-		stat := m.getStat(clientId)
-		err := server.Send(&stat)
-		m.resetStat(clientId)
-		if err != nil {
-			m.unsubscribeStatClient(clientId)
-			return err
+		select {
+		case logMsg := <-listener.logsCh:
+			event := &Event{
+				Consumer: logMsg.consumerName,
+				Method:   logMsg.methodName,
+				Host:     "127.0.0.1:8083",
+			}
+			srv.Send(event)
+
+		case <-listener.closeCh:
+			return nil
+		}
+	}
+}
+
+func (s *service) Statistics(interval *StatInterval, srv Admin_StatisticsServer) error {
+
+	closeCh := make(chan struct{})
+
+	ticker := time.NewTicker(time.Second * time.Duration(interval.IntervalSeconds))
+
+	sl := statListener{
+		statCh:  make(chan *statMsg, 0),
+		closeCh: make(chan struct{}, 0),
+	}
+
+	s.addStatListener(&sl)
+
+	c := make(map[string]uint64)
+	m := make(map[string]uint64)
+
+	for {
+		select {
+		case <-ticker.C:
+			statEvent := &Stat{
+				Timestamp:  0,
+				ByMethod:   m,
+				ByConsumer: c,
+			}
+
+			srv.Send(statEvent)
+
+			c = make(map[string]uint64)
+			m = make(map[string]uint64)
+
+		case statMsg := <-sl.statCh:
+			_, ok := c[statMsg.consumerName]
+			if !ok {
+				c[statMsg.consumerName] = 1
+			} else {
+				c[statMsg.consumerName]++
+			}
+
+			_, ok = m[statMsg.methodName]
+			if !ok {
+				m[statMsg.methodName] = 1
+			} else {
+				m[statMsg.methodName]++
+			}
+
+		case <-closeCh:
+			fmt.Println("CLOSED")
+			return nil
 		}
 	}
 }
